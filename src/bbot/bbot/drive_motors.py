ccce0a10d9053db86bf379f5e86cb62b6a58dfd1
Riley-Cameron <astoriacamfam@gmail.com>
2025-02-10 16:26:06 -0800

    updated the motor node to accept a Twist message type

--- src/bbot/bbot/drive_motors.py
+++ src/bbot/bbot/drive_motors.py
@@ -14,44 +14,39 @@
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String 
+from geometry_msgs.msg import Twist
 from pysabertooth import Sabertooth
 
-saber = Sabertooth('/dev/ttyACM0', baudrate=9600, address=128, timeout=0.1)
-saber2 = Sabertooth('/dev/ttyACM1', baudrate=9600, address=128, timeout=0.1)
+saber = Sabertooth('/dev/ttyACM0', baudrate=9600, address=128, timeout=0.1) #left wheels
+saber2 = Sabertooth('/dev/ttyACM1', baudrate=9600, address=128, timeout=0.1) #right wheels
 
 
-class KeyboardSubscriber(Node):
+class DriveMotors(Node):
 
     def __init__(self):
-        super().__init__('keyboard_subscriber')
+        super().__init__('drive_motors')
         self.subscription = self.create_subscription(
-            String,
-            'keyboard',
+            Twist,
+            'cmd/velocity',
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
-        self.kb_state = {'w': '0', 's': '0', 'a': '0', 'd': '0', 'throttle' : 100}
         self.get_logger().info('Successful initialization!')
 
     def listener_callback(self, msg):
         #wasd:throttle
-        msg = msg.data
         print(msg)
-        self.kb_state['w'] = msg[0]
-        self.kb_state['s'] = msg[1]
-        self.kb_state['a'] = msg[2]
-        self.kb_state['d'] = msg[3]
-        self.kb_state['throttle'] = int(msg[5:])
+        throttle = msg.linear.x
+        rotation = msg.angular.z
 
-        if (self.kb_state['w'] == '1'):
+        if (throttle > 0):
 
-            t1 = self.kb_state['throttle']
-            t2 = self.kb_state['throttle']
+            t1 = throttle
+            t2 = throttle
 
-            if (self.kb_state['a'] == '1'):
+            if (rotation < 0): # left turn
                 t2 = int(t2 * 0.5)
-            elif (self.kb_state['d'] == '1'):
+            elif (rotation > 0): # right turn
                 t1 = int(t2 * 0.5)
 
             saber.drive(1, t1)
@@ -59,40 +54,39 @@ class KeyboardSubscriber(Node):
             saber2.drive(1, t2)
             saber2.drive(2, -t2)
             self.get_logger().info("Forward")
-        elif (self.kb_state['s'] == '1'):
-            saber.drive(1, -self.kb_state['throttle'])
+        elif (throttle > 0):
+            saber.drive(1, -throttle)
             saber.drive(2, self.kb_state['throttle'])
             saber2.drive(1, -self.kb_state['throttle'])
             saber2.drive(2, self.kb_state['throttle'])
 
             self.get_logger().info("Backward")
-        elif (self.kb_state['a'] == '1'):
-            saber.drive(1, -self.kb_state['throttle'])
-            saber.drive(2, self.kb_state['throttle'])
-        elif (self.kb_state['d'] == '1'):
-            saber2.drive(1, -self.kb_state['throttle'])
-            saber2.drive(2, self.kb_state['throttle'])
+        elif (rotation < 0): # left
+            saber.drive(1, -throttle)
+            saber.drive(2, throttle)
+        elif (rotation > 0): # right
+            saber2.drive(1, -throttle)
+            saber2.drive(2, throttle)
 
         else:
             saber.stop()
             saber2.stop()
-            self.get_logger().info("Pin off")
+            self.get_logger().info("Stopped")
 
-        self.get_logger().info('Keyboard: "%s"' % self.kb_state)
 
 
 def main(args=None):
     
     rclpy.init(args=args)
 
-    keyboard_subscriber = KeyboardSubscriber()
+    drive_motors = DriveMotors()
 
-    rclpy.spin(keyboard_subscriber)
+    rclpy.spin(drive_motors)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    keyboard_subscriber.destroy_node()
+    drive_motors.destroy_node()
     rclpy.shutdown()
 
 
