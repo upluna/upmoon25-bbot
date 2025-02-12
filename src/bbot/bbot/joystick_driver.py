36e1010139173e3f8f54665bce8bdb6e10a77fb8
Riley-Cameron <astoriacamfam@gmail.com>
2025-02-12 14:01:46 -0800

    Added conveyor command publishing (Y button)

--- src/bbot/bbot/joystick_driver.py
+++ src/bbot/bbot/joystick_driver.py
@@ -22,6 +22,27 @@ from pygame.locals import *
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64, Int16
 
+# Controller Axes (return -1.0 to 1.0)
+LJOY_X = 0
+LJOY_Y = 1
+LBUMPER = 2
+RJOY_X = 3
+RJOY_Y = 4
+RBUMPER = 5
+
+# Controller Buttons
+A_BT = 0
+B_BT = 1
+X_BT = 2
+Y_BT = 3
+LTRIGGER = 4
+RTRIGGER = 5
+BACK_BT = 6
+START_BT = 7
+POWER_BT = 8
+LSTICK_BT = 9
+RSTICK_BT = 10
+
 class JoystickDriver(Node):
 
     def __init__(self):
@@ -47,29 +68,59 @@ class JoystickDriver(Node):
 
         self.controller = pygame.joystick.Joystick(0)
 
+        num_buttons = self.controller.get_numbuttons()
+        num_axes = self.controller.get_numaxes()
+        print("Buttons: " + str(num_buttons) + " Axes: " + str(num_axes))
+
         self.FPS = 25
 
     # Publishes joystick inputs
     def pollEvents(self):
+        # For Debugging: print all of the axes/button states
+        # print("Axis 0: " + str(self.controller.get_axis(0)))
+        # print("Axis 1: " + str(self.controller.get_axis(1)))
+        # print("Axis 2: " + str(self.controller.get_axis(2)))
+        # print("Axis 3: " + str(self.controller.get_axis(3)))
+        # print("Axis 4: " + str(self.controller.get_axis(4)))
+        # print("Axis 5: " + str(self.controller.get_axis(5)))
+        # print("Button 0: " + str(self.controller.get_button(0)))
+        # print("Button 1: " + str(self.controller.get_button(1)))
+        # print("Button 2: " + str(self.controller.get_button(2)))
+        # print("Button 3: " + str(self.controller.get_button(3)))
+        # print("Button 4: " + str(self.controller.get_button(4)))
+        # print("Button 5: " + str(self.controller.get_button(5)))
+        # print("Button 6: " + str(self.controller.get_button(6)))
+        # print("Button 7: " + str(self.controller.get_button(7)))
+        # print("Button 8: " + str(self.controller.get_button(8)))
+        # print("Button 9: " + str(self.controller.get_button(9)))
+        # print("Button 10: " + str(self.controller.get_button(10)))
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 self.destroy_node()
                 rclpy.shutdown()
                 pygame.quit()
 
-        throttle = -100 * self.controller.get_axis(1)
+
+        # Publish Velocity Command
+        throttle = -100 * self.controller.get_axis(LJOY_Y)
         if (throttle <= 5 and throttle >= -5):
             throttle = 0.0
         
-        rotation = 100 * self.controller.get_axis(3)
+        rotation = 100 * self.controller.get_axis(RJOY_X)
         if (rotation <= 5 and rotation >= -5):
             rotation = 0.0
 
-        # Publish joystick data
         msg = Twist()
         msg.linear.x = throttle
         msg.angular.z = rotation
         self.velocity_pub.publish(msg)
+
+
+        # Publish Conveyor Command
+        msg = Int16()
+        msg.data = self.controller.get_button(Y_BT)
+        self.conveyor_pub.publish(msg)
 
         pygame.display.update()
         pygame.time.Clock().tick(self.FPS)
