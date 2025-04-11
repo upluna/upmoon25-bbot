ffb03b75a01567403aba30cf45afe40cc6a14e02
Riley-Cameron <astoriacamfam@gmail.com>
2025-04-10 18:59:20 -0700

    launch fix

--- src/bbot/setup.py
+++ src/bbot/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/bbot_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/rpi_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
